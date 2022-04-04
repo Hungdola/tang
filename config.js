762061e907cc70f72a9711d69239cc84f7ace651
Hungdola <92678824+Hungdola@users.noreply.github.com>
2022-04-04 14:58:46 +0700

    virus

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Xin chào bé Thương!',
+    introDesc: `Covid không phải là một loại bệnh đáng
+    e ngại như trước nữa, Nhưng hậu covid thì sẽ khá là mệt mỏi đấy.
+    Vậy nên hãy chăm sóc sức khỏe của mình thật tốt nhé`,
+    btnIntro: 'Oke bro',
+    title: 'Bạn chắc chắn chứ ?',
+    desc: 'Sức khỏe của bạn chứ k phải của tôi đâu đấy',
+    btnYes: 'biết rồi nói lằm nói lốn',
+    btnNo: 'kệ mẹ bà',
+    question:'Lúc nào rảnh thì cứ ới bạn nhé, luôn luôn sẵn sàng nhảy dù :)',
+    btnReply: 'Gửi cmn luôn',
+    reply: 'được bạn ơi, bạn có nhập cái j thì nó cũng thế thôi kkkk',
+    mess: 'ok bro',
+    messDesc: 'chuyến bay sắp sẵn sàng',
+    btnAccept: 'Okiiiii lun',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
